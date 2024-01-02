5ac2b1f887430d2d413a77de1eb7fec86f5f75b3
Dan Giles <daniel.giles@ucdconnect.ie>
2024-01-02 10:07:08 +0000

    Adding Speedy files

---
+++ SPEEDY/speedy/model/run/run_first.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#=======================================================================
+# run_first
+#   To run the SPEEDY model for the first time. That is, you do not have
+#   a gridded initial condition, so that the model starts from the
+#   atmosphere at rest. The atmosphere at rest means zero winds
+#   everywhere and constant T with vertical profile of the standard
+#   atmosphere.
+#=======================================================================
+
+# Directory settings
+cd ../..
+SPEEDY=`pwd`
+NATURE=$SPEEDY/DATA/nature
+TMPDIR=$SPEEDY/model/tmp
+
+# Source experiment configuration
+source $SPEEDY/config.sh
+
+echo "Creating output directory"
+mkdir -p $NATURE
+cp $SPEEDY/common/$nat_res.ctl $NATURE
+
+# Work directory
+echo "Creating work directory"
+rm -rf $TMPDIR
+mkdir -p $TMPDIR
+cd $TMPDIR
+
+# 1-year spin-up
+SYYYY=$((IYYYY-1))
+SMM=$IMM
+SDD=$IDD
+SHH=$IHH
+
+echo 'Building model'
+cp $SPEEDY/model/source/makefile .
+cp $SPEEDY/model/source/*.f90 .
+cp $SPEEDY/model/source/*.h .
+cp $SPEEDY/model/source/*.s .
+
+echo "Patching configuration parameters"
+if [[ "$nat_res" = "t39" ]]
+then
+    sed -i '' -e "s/NTRUN/39/g" mod_atparam.f90
+    sed -i '' -e "s/NLON/120/g" mod_atparam.f90
+    sed -i '' -e "s/NLAT/60/g" mod_atparam.f90
+    sed -i '' -e "s/NSTEPS/72/g" mod_tsteps.f90
+elif [[ "$nat_res" = "t30" ]]
+then
+    sed -i '' -e "s/NTRUN/30/g" mod_atparam.f90
+    sed -i '' -e "s/NLON/96/g" mod_atparam.f90
+    sed -i '' -e "s/NLAT/48/g" mod_atparam.f90
+    sed -i '' -e "s/NSTEPS/36/g" mod_tsteps.f90
+fi
+sed -i '' -e "s/NMONTS/12/g" mod_tsteps.f90
+sed -i '' -e "s/NMONRS/0/g" mod_tsteps.f90
+sed -i '' -e "s/IHOUT/.true./g" mod_tsteps.f90
+sed -i '' -e "s/IPOUT/.false./g" mod_tsteps.f90
+sed -i '' -e "s/SIXHRRUN/.false./g" mod_tsteps.f90
+
+make -s imp
+sh inpfiles.s $nat_res
+
+echo "Begin spin-up from $IYYYY/$IMM/$IDD/$IHH"
+FORT2=0
+echo $FORT2 > fort.2
+echo $SYYYY >> fort.2
+echo $SMM >> fort.2
+echo $SDD >> fort.2
+echo $SHH >> fort.2
+# time ./imp | tee out.lis
+time ./imp > out.lis
+mv $IYYYY$IMM$IDD$IHH.grd $NATURE
+
+echo "Cleaning up"
+cd ..
+# rm -rf $TMPDIR
